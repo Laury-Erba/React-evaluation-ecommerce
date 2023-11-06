e2a3488346ffe319433e0e921c1ee776dd15911f
laury <lauryerba@hotmail.fr>
2023-11-06 15:32:57 +0100

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,16 @@ import Home from "./component/Home.jsx";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import Products from "./component/Products";
-import Router from "router";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/src/component/Products.jsx" component={Products} />
-        <Navbar />
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </Router>
   );
 }
 
