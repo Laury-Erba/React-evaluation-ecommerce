416f43c8ee9d306619101f98c68a29d02556c5a1
FusionFxy <axelrulier@gmail.com>
2023-11-07 13:53:45 +0100

    resolution store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
+import { configureStore } from "redux";
 import rootReducers from "./reducer";
 
-const store = createStore(rootReducers);
+const store = configureStore(rootReducers);
 
 export default store;
